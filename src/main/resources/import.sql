06cdb92cf0621b709d1a61d52983c297e86ea37a
Douglas Santos <douglas_j.santos@hotmail.com>
2024-12-20 16:40:13 -0300

    adicionado o bCrypt

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$VTY215ie1XVrtYQmgu1czuCduCtWK2T7lYGDL1eiNpUwoy/syjzzC');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
